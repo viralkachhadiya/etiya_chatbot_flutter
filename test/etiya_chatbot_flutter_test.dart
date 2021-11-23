4a0c03fc551250e29c176455e48392e5d8b788cb
Enes Karaosman <eneskaraosman53@gmail.com>
2021-11-23 14:24:26 +0300

    [FIX] Authentication related tests coded.

--- test/etiya_chatbot_flutter_test.dart
+++ test/etiya_chatbot_flutter_test.dart
@@ -10,13 +10,11 @@ import 'package:mocktail/mocktail.dart';
 
 class MockedHttpClientRepository extends Mock implements HttpClientRepository {}
 
-// @GenerateMocks([MockHttpClientRepository, HttpClientRepository])
 void main() {
   group('ChatbotCubit', () {
     late ChatbotCubit chatbotCubit;
     late EtiyaChatbotBuilder _etiyaChatbotBuilder;
     late SocketRepository _socketRepository;
-    late HttpClientRepository _httpClientRepository;
     late MockedHttpClientRepository _mockHttpClientRepository;
 
     setUp(() {
@@ -24,12 +22,6 @@ void main() {
         serviceUrl: 'https://chatbotbo-demo8.serdoo.com/api/chat',
         socketUrl: 'https://chatbotbo-demo8.serdoo.com/nlp',
         userName: 'enesKaraosman',
-      );
-
-      _httpClientRepository = FakeHttpClientRepository(
-        serviceUrl: 'serviceUrl',
-        authUrl: 'authUrl',
-        userId: 'userId',
       );
 
       _mockHttpClientRepository = MockedHttpClientRepository();
@@ -43,7 +35,6 @@ void main() {
       chatbotCubit = ChatbotCubit(
         chatbotBuilder: _etiyaChatbotBuilder,
         socketRepository: _socketRepository,
-        // httpClientRepository: httpClientRepository,
         httpClientRepository: _mockHttpClientRepository,
       );
     });
@@ -52,83 +43,51 @@ void main() {
       chatbotCubit.close();
     });
 
-    // test('Test mock', () {
-    //   when(
-    //     () => mockHttpClientRepository.auth(
-    //       username: any<String>(),
-    //       password: any<String>(),
-    //     ),
-    //   ).thenAnswer((_) async => true);
-    //   // .thenReturn(Future.value(true));
-    //
-    //   // expect(mockHttpClientRepository.auth(username: 'username', password: 'password'), true);
-    //   // expectLater(() => mockHttpClientRepository.auth(username: 'username', password: 'password'), true);
-    //   expectLater(
-    //       mockHttpClientRepository.auth(
-    //           username: 'username', password: 'password'),
-    //       true);
-    // });
-
     test('Should_InitialStateEqual', () {
       final _chatbotCubit = ChatbotCubit(
         chatbotBuilder: _etiyaChatbotBuilder,
         socketRepository: _socketRepository,
-        httpClientRepository: _httpClientRepository,
+        httpClientRepository: _mockHttpClientRepository,
       );
       expect(_chatbotCubit.state, ChatbotMessages());
       _chatbotCubit.close();
     });
 
-    test('Boom', () {
-      when(
-            () => _mockHttpClientRepository.auth(
-          // () => _mockHttpClientRepository.auth(
-          username: any<String>(named: 'username'),
-          password: any<String>(named: 'password'),
-        ),
-      ).thenAnswer((_) async => true);
-      expect(_mockHttpClientRepository.auth(username: any(named: 'username'), password: any(named: 'password')), true);
-    });
-
     blocTest<ChatbotCubit, ChatbotState>(
-      'emit ChatbotUserAuthenticated(true) when user tries to authenticate',
+      'emit ChatbotUserAuthenticated(true) when user authenticates successfully',
       build: () {
         when(
-          () => chatbotCubit.httpClientRepository.auth(
-          // () => _mockHttpClientRepository.auth(
-            username: any<String>(named: 'username'),
-            password: any<String>(named: 'password'),
+          () => _mockHttpClientRepository.auth(
+            username: 'username',
+            password: 'correctPassword',
           ),
         ).thenAnswer((_) async => true);
-
-        // return ChatbotCubit(
-        //   chatbotBuilder: _etiyaChatbotBuilder,
-        //   socketRepository: _socketRepository,
-        //   httpClientRepository: _mockHttpClientRepository,
-        // );
         return chatbotCubit;
       },
       act: (bloc) => bloc.authenticate(
-        username: any<String>(named: 'username'), //'username',
-        password: any<String>(named: 'password'), //'password',
+        username: 'username',
+        password: 'correctPassword',
       ),
       expect: () => contains(ChatbotUserAuthenticated(true)),
     );
 
-    // blocTest<ChatbotCubit, ChatbotState>(
-    //   'emits [ChatbotUserAuthenticated(true)] when ChatbotCubit authenticates successfully',
-    //   build: () => chatbotCubit,
-    //   act: (bloc) =>
-    //       bloc.authenticate(username: 'username', password: 'password'),
-    //   expect: () => contains(ChatbotUserAuthenticated(true)),
-    // );
-    //
-    // blocTest<ChatbotCubit, ChatbotState>(
-    //   'emits [ChatbotUserAuthenticated(false)] when ChatbotCubit authenticates unsuccessfully',
-    //   build: () => chatbotCubit,
-    //   act: (bloc) =>
-    //       bloc.authenticate(username: 'username', password: 'wrongPassword'),
-    //   expect: () => contains(ChatbotUserAuthenticated(false)),
-    // );
+    blocTest<ChatbotCubit, ChatbotState>(
+      'emit ChatbotUserAuthenticated(false) when user authentication fails',
+      build: () {
+        when(
+              () => _mockHttpClientRepository.auth(
+            username: 'username',
+            password: 'wrongPassword',
+          ),
+        ).thenAnswer((_) async => false);
+        return chatbotCubit;
+      },
+      act: (bloc) => bloc.authenticate(
+        username: 'username',
+        password: 'wrongPassword',
+      ),
+      expect: () => contains(ChatbotUserAuthenticated(false)),
+    );
+
   });
 }
