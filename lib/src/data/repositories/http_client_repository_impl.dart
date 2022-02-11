4493adb87152145cebd639a3bedb032ce0dd391f
Enes Karaosman <eneskaraosman53@gmail.com>
2022-02-11 08:01:32 +0300

    [MOD] TOGGMobileSDK http client usage implemented.

--- lib/src/data/repositories/http_client_repository_impl.dart
+++ lib/src/data/repositories/http_client_repository_impl.dart
@@ -4,32 +4,38 @@ import 'package:etiya_chatbot_flutter/src/data/models/models.dart';
 import 'package:etiya_chatbot_flutter/src/domain/http_client_repository.dart';
 import 'package:etiya_chatbot_flutter/src/util/logger.dart';
 import 'package:http/http.dart' as http;
+import 'package:togg_mobile_super_app_sdk/togg_mobile_super_app_sdk.dart';
 
 class HttpClientRepositoryImpl extends HttpClientRepository {
   HttpClientRepositoryImpl({
     required String serviceUrl,
     required String authUrl,
     required String userId,
-  }) : super(serviceUrl: serviceUrl, authUrl: authUrl, userId: userId);
+  }) : super(serviceUrl: serviceUrl, authUrl: authUrl, userId: userId) {
+    _httpClient = TOGGMobileSdk().getTOGGHttpClient();
+  }
+
+  late final ITOGGHttpClient _httpClient;
 
   @override
   Future<bool> auth({
     required String username,
     required String password,
   }) async {
-    final url = Uri.parse(authUrl);
     try {
-      final response = await http.post(
-        url,
+      final _response = await _httpClient.post(
+        endpoint: authUrl,
         headers: <String, String>{
           'Content-Type': 'application/json',
         },
-        body: json.encode({
+        params: {
           "username": username,
           "password": password,
           "chatId": "mobile:$userId"
-        }),
+        },
       );
+      final response = _response as http.Response?;
+      if (response == null) return false;
       if (response.statusCode >= 200 && response.statusCode <= 300) {
         Log.info(response.body);
         final json = jsonDecode(response.body) as Map<String, dynamic>;
@@ -51,13 +57,15 @@ class HttpClientRepositoryImpl extends HttpClientRepository {
   @override
   Future<void> sendMessage(MessageRequest request) async {
     try {
-      final response = await http.post(
-        Uri.parse('$serviceUrl/mobile'),
+      final _response = await _httpClient.post(
+        endpoint: '$serviceUrl/mobile',
         headers: <String, String>{
           'Content-Type': 'application/json',
         },
-        body: jsonEncode(request.toJson()),
+        params: request.toJson(),
       );
+      final response = _response as http.Response?;
+      if (response == null) return;
       if (response.statusCode >= 200 && response.statusCode <= 300) {
         Log.info("User's message sent successfully");
       } else {
